8038738b497323b938228c741eca6382ca264e2d
Braden Griebel <bgriebel@uw.edu>
2025-12-02 13:31:33 -0800

    Adding tests for gene target density

--- tests/network/test_density.py
+++ tests/network/test_density.py
@@ -1,16 +1,21 @@
 # Standard Library Imports
+import pathlib
 import unittest
 
 # External Imports
+import cobra
 import networkx as nx
 import numpy as np
 import pandas as pd
 
 # Local Imports
+from metworkpy import read_model
+from metworkpy.network import create_metabolic_network, bipartite_project
 from metworkpy.network.density import (
     label_density,
     find_dense_clusters,
     _node_density,
+    gene_target_density,
 )
 
 
@@ -108,6 +113,72 @@ class TestFindDenseClusters(unittest.TestCase):
         self.assertAlmostEqual(res_df.loc[5, "density"], 3)
         self.assertAlmostEqual(res_df.loc[7, "density"], 2)
         self.assertNotEqual(res_df.loc[5, "cluster"], res_df.loc[0, "cluster"])
+
+
+class TestGeneTargetDensity(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cobra.Configuration().solver = "glpk"
+        cls.data_path = (
+            pathlib.Path(__file__).parent.parent.absolute() / "data"
+        )
+        cls.model = read_model(cls.data_path / "test_model.xml")
+        metabolic_network = create_metabolic_network(
+            model=cls.model,
+            weighted=False,
+            directed=True,
+        )
+        cls.reaction_network = bipartite_project(
+            metabolic_network,
+            node_set=cls.model.reactions.list_attr("id"),
+        )
+
+    def test_gene_target_density_r0(self):
+        test_net = self.reaction_network
+        test_model = self.model
+        # Test with single gene label
+        gene_targets = ["g_A_B_D_E"]
+        # Perform the gene target density
+        test_density = gene_target_density(
+            metabolic_network=test_net,
+            metabolic_model=test_model,
+            gene_labels=gene_targets,
+            radius=0,
+        )
+        # Since there is only one targeted gene, with a radius of 0,
+        # every reaction but r_A_B_D_E should have a density of 0
+        # and r_A_B_D_E should have a density of 1.0
+        for rxn, density in test_density.items():
+            if rxn == "r_A_B_D_E":
+                self.assertAlmostEqual(density, 1.0, delta=1e-7)
+            else:
+                self.assertAlmostEqual(density, 0.0, delta=1e-7)
+
+    def test_gene_target_density_r1(self):
+        test_net = self.reaction_network
+        test_model = self.model
+        # Test with single gene label
+        gene_targets = ["g_A_B_D_E"]
+        # Perform the gene target density
+        test_density = gene_target_density(
+            metabolic_network=test_net,
+            metabolic_model=test_model,
+            gene_labels=gene_targets,
+            radius=1,
+        )
+        # Since there is only one targeted gene, with a radius of 0,
+        # every reaction but r_A_B_D_E should have a density of 0
+        # and r_A_B_D_E should have a density of 1.0
+        print(test_density)
+        for rxn, density in test_density.items():
+            if rxn == "r_A_B_D_E" or rxn == "r_C_E_F":
+                self.assertAlmostEqual(density, 0.2, delta=1e-7)
+            elif rxn == "R_A_imp" or rxn == "R_B_imp":
+                self.assertAlmostEqual(density, 0.5, delta=1e-7)
+            elif rxn == "r_D_G":
+                self.assertAlmostEqual(density, 1.0 / 3.0, delta=1e-7)
+            else:
+                self.assertAlmostEqual(density, 0.0, delta=1e-7)
 
 
 if __name__ == "__main__":
